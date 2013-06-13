56b4dfa3f66b9d339ec4c1a0a63cbacaa6ba06db
Vous <siddhartha.duri@gmail.com>
2013-06-13 15:11:28 -0400

    Updated Game Models

--- game-models.py
+++ game-models.py
@@ -67,6 +67,7 @@ class NationalLegislator(BaseModel):
 	state = ForeignKeyField(State, "national_seats")
 	# Some seats may become inactive due to population shifts
 	active = BooleanField(default = True)
+	next_election = DateField() # When is the next election in this seat?
 
 class StateLegislator(BaseModel):
 	# Not the person, the office
@@ -76,6 +77,7 @@ class StateLegislator(BaseModel):
 	county = ForeignKeyField(County, "state_seats")
 	# Some seats may become inactive due to population shifts
 	active = BooleanField(default = True)
+	next_election = DateField() # When is the next election in this seat?
 
 class CountyLegislator(BaseModel):
 	# Not the person, the office
@@ -85,6 +87,7 @@ class CountyLegislator(BaseModel):
 	county = ForeignKeyField(County, "county_seats")
 	# Some seats may become inactive due to legislative actions
 	active = BooleanField(default = True)
+	next_election = DateField() # When is the next election in this seat?
 
 class MunicipalityLegislator(BaseModel):
 	# Not the person, the office
@@ -94,3 +97,39 @@ class MunicipalityLegislator(BaseModel):
 	municipality = ForeignKeyField(Municipality, "municipality_seats")
 	# Some seats may become inactive due to legislative actions
 	active = BooleanField(default = True)
+	next_election = DateField() # When is the next election in this seat?
+
+class StateParty(BaseModel):
+	# The party itself
+	#### THERE SHOULD BE A REFERENCE TO THE USER
+	name = CharField()
+	status = IntegerField() # 1 - unrecognized state, 2 = recognized state
+	funds = DecimalField(decimal_places = 2)
+	state = ForeignKeyField(States, "parties") # What states it is open in 
+
+class NationalParty(BaseModel):
+	# The party itself
+	#### THERE SHOULD BE A REFERENCE TO THE USER
+	name = CharField()
+	status = IntegerField() # 1 - unrecognized state, 2 = recognized state
+	funds = DecimalField(decimal_places = 2)
+	nation = ForeignKeyField(Nation, "parties") # What states it is open in 
+
+class Politician(BaseModel):
+	name = CharField()
+	funds = DecimalField(decimal_places = 2)
+	party = ForeignKeyField(Party, "politicians")
+	econ_freedom = DecimalField(decimal_places = 2)
+	civil_freedom = DecimalField(decimal_places = 2)
+
+class MunicipalityLegislatorPerson(Politician):
+	office = ForeignKeyField(MunicipalityLegislator, "politician")
+
+class CountyLegislatorPerson(Politician):
+	office = ForeignKeyField(CountyLegislator, "politician")
+
+class StateLegislatorPerson(Politician):
+	office = ForeignKeyField(StateLegislator, "politician")
+
+class NationalLegislatorPerson(Politician):
+	office = ForeignKeyField(NationalLegislator, "politician")
